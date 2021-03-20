7e8efb2b49235c534c315cbad83af7112f229352
Leo <leo.queiroz90@gmail.com>
2021-03-20 17:17:22 -0300

    Calculadora

---
+++ 01_RestWithASPNET_Calculadora/Calculadora/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadoraController : ControllerBase
+    {
+        private readonly ILogger<CalculadoraController> _logger;
+
+        public CalculadoraController(ILogger<CalculadoraController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        
+        public IActionResult Sum( string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+    }
+}
